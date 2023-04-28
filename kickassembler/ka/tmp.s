99f9628b6f8e7891771481ddce8ef46eaf32069d
Matt Stine <matt.stine@gmail.com>
2023-04-28 10:27:31 -0500

    add rta macro

--- kickassembler/ka/tmp.s
+++ kickassembler/ka/tmp.s
@@ -2,3 +2,19 @@
    .text string
    .byte 0 
 }
+
+.macro rta(wordList){
+   .for(var i = 0; i < wordList.size(); i++) {
+      .var nextWord = wordList.get(i)
+      .eval nextWord--
+      .byte <nextWord,>nextWord
+   }
+}
+
+.assert "rta generates decremented words in hi/lo byte order",{
+   rta(List().add($fce2,$d012))
+},
+{
+   .byte $e1,$fc
+   .byte $11,$d0
+}
