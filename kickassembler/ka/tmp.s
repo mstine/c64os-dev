a306fa09b6f5270f3b3a9110f7292ea6ff4180f8
Matt Stine <matt.stine@gmail.com>
2023-04-28 20:59:19 -0500

    add docs and #importonce

--- kickassembler/ka/tmp.s
+++ kickassembler/ka/tmp.s
@@ -1,8 +1,27 @@
+#importonce
+
+/*
+ * nullStr
+ * 
+ * Provides the equivalent of .null in TMP/TMPx. Creates null-terminated
+ * .strings.
+ *
+ * string -> the .text to insert with a null-terminator
+ */
 .macro nullStr(string) {
    .text string
    .byte 0 
 }
 
+/*
+ * rta
+ * 
+ * Provides the equivalent of .rta in TMP/TMPx. Creates a .byte
+ * table from a List of .word. Each .word is decremented and appended
+ * in lo/hi-byte order.
+ *
+ * wordList -> a KickAssembler List of .word
+ */
 .macro rta(wordList){
    .for(var i = 0; i < wordList.size(); i++) {
       .var nextWord = wordList.get(i)
